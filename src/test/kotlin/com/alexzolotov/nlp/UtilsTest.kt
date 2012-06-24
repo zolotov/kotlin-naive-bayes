a1b723d64376ab1714931ac96348c759c0e9cfa6
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2012-06-24 13:25:15 +0400

    Add test for utility functions

---
+++ src/test/kotlin/com/alexzolotov/nlp/UtilsTest.kt
@@ -0,0 +1,34 @@
+package com.alexzolotov.nlp
+
+import java.util.List
+import kotlin.test.expect
+import org.testng.annotations.Test as test
+
+class UtilsTest {
+
+    test fun flatten() {
+        expect(arrayList(1, 2, 3, 4)) {
+            arrayList<List<Int>>(arrayList(1, 2), arrayList(3, 4)).flatten()
+        }
+    }
+
+    test fun sortBy() {
+        expect(arrayList(1 to 3, 2 to 2)) {
+            arrayList(2 to 2, 1 to 3).sortBy { it._2 }
+        }
+        expect(arrayList(2 to 2, 1 to 3)) {
+            arrayList(2 to 2, 1 to 3).sortBy { it._1 }
+        }
+    }
+
+    test fun appendToMap() {
+        val map = hashMap(1 to 2)
+        expect(hashMap(1 to 2, 3 to 4)) {
+            map + #(3, 4)
+        }
+        expect(hashMap(1 to 3)) {
+            map + (1 to 3)
+        }
+    }
+}
+
